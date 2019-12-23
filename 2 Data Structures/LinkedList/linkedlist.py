ca0c8298a1ee1bb09de3bd0d7d6d1c1d1f0b8930
Mannuel Ferreira <mannuel.ferreir@gmail.com>
2019-12-23 10:02:26 +0100

    delete function

--- 2 Data Structures/LinkedList/linkedlist.py
+++ 2 Data Structures/LinkedList/linkedlist.py
@@ -26,14 +26,14 @@ class LinkedList(object):
         return self.count
 
     def insert(self, data):
-        # TODO: insert a new node
+        # insert a new node
         new_node = Node(data)
         new_node.set_next(self.head)
         self.head = new_node
         self.count += 1
     
     def find(self, val):
-        # TODO: find the first item with a given value
+        # find the first item with a given value
         item = self.head
         while (item != None):
             if item.get_data() == val:
@@ -43,12 +43,41 @@ class LinkedList(object):
         return None
 
     def deleteAt(self, idx):
-        # TODO: delete an item at given index
+        # delete an item at given index
         if idx > self.count-1:
             return
+        if idx == 0:
+            self.head = self.head.get_next()
+        else:
+            tempIdx = 0
+            node = self.head
+            while tempIdx < idx - 1:
+                node = node.get_next()
+                tempIdx += 1
+            node.set_next(node.get_next().get_next())
+            self.count -= -1
 
     def dump_list(self):
         tempnode = self.head
         while (tempnode != None):
             print ("Node: ", tempnode.get_data())
             tempnode = tempnode.get_next()
+
+# create a linked list and insert some items
+itemList = LinkedList()
+itemList.insert(38)
+itemList.insert(49)
+itemList.insert(13)
+itemList.insert(15)
+itemList.dump_list()
+
+# exercise the list
+# print("Item count: ", itemList.get_count())
+# print("Finding count: ", itemList.find(13))
+# print("Finding count: ", itemList.find(78))
+
+# delete an item
+itemList.deleteAt(3)
+print("Item count: ", itemList.get_count())
+print("Finding item: ", itemList.find(38))
+itemList.dump_list()
