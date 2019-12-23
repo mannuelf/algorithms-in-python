bb88163b6a0a1cd82922f70f5cd36443db21b055
Mannuel Ferreira <mannuel.ferreir@gmail.com>
2019-12-23 09:09:41 +0100

    LinkedList

---
+++ 2 Data Structures/LinkedList/linkedlist.py
@@ -0,0 +1,36 @@
+# the Node class
+class Node(object):
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+    def get_data(self):
+        return self.val
+
+    def set_data(self, val):
+        self.val = val
+
+    def get_next(self):
+        return self.next
+
+    def set_next(self, next):
+        self.next = next
+
+# the LinkedList class
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+        self.count = 0
+
+    def get_count(self):
+        return self.count
+
+    def insert(self, data):
+        # TODO: insert a new node
+        new_node = Node(data)
+    
+    def find(self, val):
+        # TODO: find the first item with a given value
+        item = self.head
+
+        return None
